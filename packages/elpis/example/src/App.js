4f96f2c5102096eb5e78d175f735e3c79ef0dbc8
Kyle Bradford <kfoxb@kfoxb.com>
2018-08-28 01:16:13 -0600

    WIP: stopping point
    affects: elpis

--- packages/elpis/example/src/App.js
+++ packages/elpis/example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'elpis'
+import Elpis from 'elpis'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Elpis />
       </div>
     )
   }
